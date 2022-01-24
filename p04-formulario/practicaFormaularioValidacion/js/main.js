3e9f198cb469b0814f9980715744cfce8ecbacf0
Jose Esteves <josefelixestevenpacheco@gmail.com>
2022-01-23 21:18:45 -0400

    manera optimizada por jose

--- p04-formulario/practicaFormaularioValidacion/js/main.js
+++ p04-formulario/practicaFormaularioValidacion/js/main.js
@@ -3,38 +3,52 @@
 const form = document.getElementById('form');
 const contentInputUser = document.getElementById('contentInputUser');
 const contentInputEmail = document.getElementById('contentInputEmail');
-const userName = document.getElementById('userName');
-const userEmail = document.getElementById('userEmail');
 const formMsgUser = document.getElementById('formMsgUser');
 const submit = document.getElementById('submit');
 const formMsgEmail = document.getElementById('formMsgEmail');
-const regUserName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
-const regUserEmail =
-	/^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/;
+const validacion = {
+	UserName: /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/,
+	UserEmail:
+		/^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/,
+};
 
 form.addEventListener('submit', e => {
-	if (regUserName.test(userName.value)) {
-		contentInputUser.classList.remove('form__content--iconErr');
-		contentInputUser.classList.add('form__content--iconCheck');
-		formMsgUser.classList.remove('form__msg--show');
-	} else {
-		contentInputUser.classList.remove('form__content--iconCheck');
-		contentInputUser.classList.add('form__content--iconErr');
-		formMsgUser.classList.add('form__msg--show');
-	}
-	if (regUserEmail.test(userEmail.value)) {
-		contentInputEmail.classList.remove('form__content--iconErr');
-		contentInputEmail.classList.add('form__content--iconCheck');
-		formMsgEmail.classList.remove('form__msg--show');
-	} else {
-		contentInputEmail.classList.remove('form__content--iconCheck');
-		contentInputEmail.classList.add('form__content--iconErr');
-		formMsgEmail.classList.add('form__msg--show');
-	}
-	if (regUserName.test(userName.value) && regUserEmail.test(userEmail.value)) {
+	e.preventDefault();
+	const { userName, userEmail } = e.target;
+	functionValidacion(
+		validacion.UserName,
+		userName.value,
+		contentInputUser,
+		formMsgUser
+	);
+	functionValidacion(
+		validacion.UserEmail,
+		userEmail.value,
+		contentInputEmail,
+		formMsgEmail
+	);
+	if (
+		validacion.UserName.test(userName.value) &&
+		validacion.UserEmail.test(userEmail.value)
+	) {
 		submit.classList.add('form__content--submit--show');
-	}else{
+	} else {
 		submit.classList.remove('form__content--submit--show');
 	}
-	e.preventDefault();
 });
+const functionValidacion = (
+	exprecion,
+	valorAValidar,
+	contentInputs,
+	formMsg
+) => {
+	if (exprecion.test(valorAValidar)) {
+		contentInputs.classList.remove('form__content--iconErr');
+		contentInputs.classList.add('form__content--iconCheck');
+		formMsgEmail.classList.remove('form__msg--show');
+	} else {
+		contentInputs.classList.remove('form__content--iconCheck');
+		contentInputs.classList.add('form__content--iconErr');
+		formMsg.classList.add('form__msg--show');
+	}
+};

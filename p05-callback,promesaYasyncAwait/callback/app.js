b1ca975de379e51b40ab85cb48323af6ffa9be3d
Jose Esteves <josefelixestevenpacheco@gmail.com>
2022-01-25 15:50:21 -0400

    practica terminada. no lo ise como bluuwed proq no lo entendi

---
+++ p05-callback,promesaYasyncAwait/callback/app.js
@@ -0,0 +1,22 @@
+/** @format */
+
+const posts = [
+	{
+		userId: 1,
+		id: 1,
+		title: 'usuario 1',
+		body: 'usuario1',
+	},
+	{
+		userId: 1,
+		id: 2,
+		title: 'usuario 1',
+		body: 'usuario1',
+	},
+	{
+		userId: 1,
+		id: 3,
+		title: 'usuario 1',
+		body: 'usuario1',
+	},
+];

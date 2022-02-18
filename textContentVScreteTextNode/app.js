65cf458efe8e5327240c399695d3ec27bebc6e93
Jose Esteven <josefelixestevenpacheco@gmail.com>
2022-02-18 00:28:46 -0400

    se cmbiara para q sea mejor

--- textContentVScreteTextNode/app.js
+++
@@ -1,7 +0,0 @@
-const h2textContent = document.getElementById('h2textContent')
-console.log(h2textContent);
-h2textContent.textContent = 'texto con text content'
-const h2nodeText = document.getElementById('h2nodeText')
-console.log(h2nodeText);
-const text = document.createTextNode(' text con text node')
-h2nodeText.appendChild(text)

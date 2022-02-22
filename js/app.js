171ae7bbb380deb839c37d9702e48bb2c7ef1a83
Jose Esteven <josefelixestevenpacheco@gmail.com>
2022-02-22 01:23:04 -0400

    export manera 1

---
+++ js/app.js
@@ -0,0 +1,6 @@
+import { sandia, platono, ceresas, Frutas } from './frutas.js';
+console.log(sandia);
+platono();
+ceresas();
+const fresa = new Frutas('🍓');
+console.log(fresa);
---
+++ js/frutas.js
@@ -0,0 +1,12 @@
+export const sandia = '🍉';
+export function platono() {
+	console.log('🍌');
+}
+export const ceresas = () => {
+	console.log('🍒');
+};
+export class Frutas {
+	constructor(nombre) {
+		this.nombre = nombre;
+	}
+}
